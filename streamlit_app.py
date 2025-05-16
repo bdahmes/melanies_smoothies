79a24a8e9351decdf19fb35becb9c156133ff2e0
bdahmes <bdahmes@users.noreply.github.com>
2025-05-16 13:50:17 -0500

    Update streamlit_app.py
    switching API call to search_on from fruit_chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,8 @@ st.write(f'The name on the smoothie will be: {name_on_order}')
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
+pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:',
@@ -22,11 +23,13 @@ ingredients_list = st.multiselect(
     max_selections=5
 )
 if ingredients_list:
-    ingredients_string = ' '.join(ingredients_list)
+    ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.subheader(f'{fruit_chosen} Nutrition Information:')
-        smoothiefroot_response = requests.get(f'https://my.smoothiefroot.com/api/fruit/{fruit_chosen}')
+        smoothiefroot_response = requests.get(f'https://my.smoothiefroot.com/api/fruit/{search_on}')
         sf_df = st.dataframe(smoothiefroot_response.json(),use_container_width=True)
         
         my_insert_stmt = f""" insert into smoothies.public.orders(ingredients,name_on_order)
