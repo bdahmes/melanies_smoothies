5a896e226b6a140631f5458249aeabe9c2d35124
bdahmes <bdahmes@users.noreply.github.com>
2025-05-27 13:18:51 -0500

    moving out of for loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,11 +34,11 @@ if ingredients_list:
         # smoothiefroot_response = requests.get(f'https://my.smoothiefroot.com/api/fruit/{search_on}')
         sf_df = st.dataframe(smoothiefroot_response.json(),use_container_width=True)
         
-        my_insert_stmt = f""" insert into smoothies.public.orders(ingredients,name_on_order)
-                values ('{ingredients_string}','{name_on_order}')"""
-        time_to_insert = st.button('Submit Order')
-        if time_to_insert:
-            session.sql(my_insert_stmt).collect()
-            st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+    my_insert_stmt = f""" insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('{ingredients_string}','{name_on_order}')"""
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
 
 
